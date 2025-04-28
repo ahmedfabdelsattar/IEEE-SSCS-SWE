b16f3573a479b2b7c0628b0ca7b395353594e0de
ahmedfabdelsattar <ahmedfabdelsattar@gmail.com>
2025-04-28 19:15:44 +0300

    task-3 has been added

---
+++ 0x01-session/3-signal_interference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int signal_strength = 0;
+
+void boost_signal(int n)
+{
+    int signal_strength = 100;
+
+    printf("booster: signal strength = %d\n", n);
+}
+
+int main()
+{
+    int n = 50;
+    
+    printf("%d\n", signal_strength);
+
+    boost_signal(n);
+
+    printf("%d\n", signal_strength);
+}
