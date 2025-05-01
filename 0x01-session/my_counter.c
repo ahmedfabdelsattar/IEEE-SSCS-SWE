b87c291d9d9bfdcb20937782840e51ec4609d2fd
ahmedfabdelsattar <ahmedfabdelsattar@gmail.com>
2025-05-01 14:56:25 +0300

    task-13 has benn added

--- 0x01-session/my_counter.c
+++ 0x01-session/my_counter.c
@@ -3,12 +3,20 @@
 int my_counter(char *c)
 {
     int count = 0;
+    int word = 0;
+
     for (int i = 0; c[i] != '\0'; i++)
     {
-        if (c[i] == ' ')
+        if (c[i] != ' ' && word == 0)
         {
+            word = 1;
             count++;
         }
+        else if (c[i] == ' ')
+        {
+            word = 0;
+        }
     }
+
     return count;
 }
