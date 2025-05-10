a98ef05b28249fa10fa0e4b04587bf02e03f7c87
ahmedfabdelsattar <ahmedfabdelsattar@gmail.com>
2025-05-10 11:56:29 +0300

    task 4 has been modified

--- 0x03-memory/4-reset.c
+++ 0x03-memory/4-reset.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-
-void access_one(int *x)
+int count_one = 0;
+int count_two = 0;
+void access_one(int *x, int *y)
 {
     *x += 1;
+    *y += 1;
 }
-void access_two(int *x)
+void access_two(int *x, int *y)
 {
     *x += 1;
+    *y += 1;
 }
 int main()
 {
     int counter = 0;
-    access_one(&counter);
-    access_two(&counter);
-    access_one(&counter);
-    access_two(&counter);
-    access_one(&counter);
-    access_two(&counter);
-    access_one(&counter);
-    access_two(&counter);
-    printf("%d\n", counter);
+    access_one(&counter, &count_one);
+    access_two(&counter, &count_two);
+    access_one(&counter, &count_one);
+    access_two(&counter, &count_two);
+    access_one(&counter, &count_one);
+    access_two(&counter, &count_two);
+    access_one(&counter, &count_one);
+    access_two(&counter, &count_two);
+    printf("both fuctions has been called %d times\n", counter);
+    printf("first fuction has been called %d times\n", count_one);
+    printf("second fuction has been called %d times\n", count_two);
 }
